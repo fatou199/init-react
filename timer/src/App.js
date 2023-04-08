378df9c1be4587d83b2cc4b2488c61d54b256d84
fatou199 <65873642+fatou199@users.noreply.github.com>
2023-04-08 15:25:12 +0200

    afficher une liste dans react

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,12 +1,29 @@
 // importer les modules dont on a besoin pour ce composant
 import React, { Component }  from 'react';
 
+const list = [
+  {id: "1", nom:"Sophine", age: "12"},
+  {id: "2", nom:"Marco", age: "19"},
+  {id: "3", nom:"Paulo", age: "29"},
+  {id: "4", nom:"Mathilde", age: "34"},
+];
+
 // Créer le composant
 class App extends Component {
+
+  renderList(){
+    return list.map(item => {
+      return (
+        <h3 key={item.id}>{item.nom} a {item.age} ans</h3>
+      )
+    })
+  }
+
   render(){
     return (
       <div>
         <p> Je suis le composant App </p>
+        {this.renderList()}
       </div>
     )
   }
