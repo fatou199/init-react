3e75fe768498347043204cf814be3fb9a8b5f6c1
fatou199 <65873642+fatou199@users.noreply.github.com>
2023-04-25 14:56:50 +0200

    composant TimerForm

--- timer/src/components/TimerForm.js
+++ timer/src/components/TimerForm.js
@@ -3,9 +3,29 @@ import React, { Component } from "react";
 class TimerForm extends Component{
 
     render(){
+        const submitText = this.props.title ? 'Modifier' : 'Ajouter';
         return(
-        <div>
-            <p>TimerForm</p>
+        <div className="form">
+            <div className="form--content">
+                <div className="form--item">
+                    <label>Title</label>
+                    <input 
+                        type="text" 
+                        placeholder="Mon titre" 
+                    />
+                </div>
+                <div className="form--item">
+                    <label>Projet</label>
+                    <input 
+                        type="text" 
+                        placeholder="Mon projet" 
+                    />
+                </div>
+            </div>
+            <div className="form--button">
+                <button className="button btn--submit">{submitText}</button>
+                <button className="button btn--cancel">Annuler</button>
+            </div>
         </div>
         )
     }
